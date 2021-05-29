0da36d709ef8e21ce5ff7536191be4c4367f1ec7
Berkes Andras <andras.berkes@mail.com>
2021-05-30 00:06:20 +0200

    Fixed cache synchronization

--- src/main/java/io/datatree/dom/Cache.java
+++ src/main/java/io/datatree/dom/Cache.java
@@ -19,7 +19,9 @@ package io.datatree.dom;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.locks.StampedLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.WriteLock;
 
 /**
  * Simple and fast memory cache. This implementation uses optimistic locking for
@@ -36,26 +38,12 @@ public class Cache<K, V> {
 
 	// --- INTERNAL VARIABLES ---
 
-	protected final StampedLock lock = new StampedLock();
+	protected final ReadLock readLock;
+	protected final WriteLock writeLock;
 
 	protected final LinkedHashMap<K, V> map;
 
 	// --- CONSTRUCTORS ---
-
-	/**
-	 * Creates a cache.
-	 * 
-	 * @param capacity
-	 *            maximum capacity of the cache
-	 * @param fair
-	 *            unused parameter
-	 * 
-	 * @deprecated do not use the "fair" parameter (it's an unnecessary
-	 *             parameter)
-	 */
-	public Cache(int capacity, boolean fair) {
-		this(capacity);
-	}
 
 	/**
 	 * Creates a cache with the specified capacity.
@@ -65,7 +53,7 @@ public class Cache<K, V> {
 	 */
 	public Cache(int capacity) {
 
-		// Insertion-order is thread safe, access-order is not
+		// Create Map
 		map = new LinkedHashMap<K, V>(capacity, 1.0f, false) {
 
 			private static final long serialVersionUID = 5994447707758047152L;
@@ -74,73 +62,55 @@ public class Cache<K, V> {
 				return size() > capacity;
 			};
 		};
+
+		// Create locks
+		ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+		readLock = lock.readLock();
+		writeLock = lock.writeLock();
 	}
 
 	// --- GET / PUT / ETC ---
 
 	public V get(K key) {
 		V value = null;
-		long stamp = lock.tryOptimisticRead();
-		if (stamp != 0) {
-			try {
-				value = map.get(key);
-			} catch (Exception modified) {
-				stamp = 0;
-			}
-		}
-		if (!lock.validate(stamp) || stamp == 0) {
-			stamp = lock.readLock();
-			try {
-				value = map.get(key);
-			} finally {
-				lock.unlockRead(stamp);
-			}
+		readLock.lock();
+		try {
+			value = map.get(key);
+		} finally {
+			readLock.unlock();
 		}
 		return value;
 	}
 
 	public void put(K key, V value) {
-		final long stamp = lock.writeLock();
+		writeLock.lock();
 		try {
 			map.put(key, value);
 		} finally {
-			lock.unlockWrite(stamp);
+			writeLock.unlock();
 		}
 	}
 
 	public void remove(K key) {
-		final long stamp = lock.writeLock();
+		writeLock.lock();
 		try {
 			map.remove(key);
 		} finally {
-			lock.unlockWrite(stamp);
+			writeLock.unlock();
 		}
 	}
 
 	public void clear() {
-		final long stamp = lock.writeLock();
+		writeLock.lock();
 		try {
 			map.clear();
 		} finally {
-			lock.unlockWrite(stamp);
+			writeLock.unlock();
 		}
 	}
 
 	public int size() {
-		int count = 0;
-		long stamp = lock.tryOptimisticRead();
-		if (stamp != 0) {
-			count = map.size();
-		}
-		if (!lock.validate(stamp) || stamp == 0) {
-			stamp = lock.readLock();
-			try {
-				count = map.size();
-			} finally {
-				lock.unlockRead(stamp);
-			}
-		}
-		return count;
+		return map.size();
 	}
 
 }
